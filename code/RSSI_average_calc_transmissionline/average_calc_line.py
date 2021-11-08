3bbddda1a33013f142e8b88f31a9ea1aea5c338b
farzan-dehbashi <farzan.dehbashi95@gmail.com>
2021-11-08 13:58:51 -0500

    .

---
+++ code/RSSI_average_calc_transmissionline/average_calc_line.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+
+file_names = np.arange(start=0, stop=16+1, step=1)
+chip_name = '0xf902f920'
+
+d = { 'average': []}
+mean_df = pd.DataFrame(data=d)
+
+for file_name in file_names: #file name is just a int
+    df = pd.read_csv('TH_'+str(file_name)+'.csv', names=["EPCValue", "TimeStamp", "RunNum", "RSSI", "Reader", "Frequency", "Power", "Antenna"])
+
+    filt = (df['EPCValue'] == chip_name)
+    filtered_df = df.loc[filt]
+
+    RSSIs = filtered_df['RSSI']
+    RSSIs = pd.to_numeric(RSSIs)
+    mean_df.at[file_name, 'average'] = RSSIs.describe().loc['mean']
+    mean_df.at[file_name, 'std'] = RSSIs.describe().loc['std']
+    #mean_df.at[file_name, 'count'] = RSSIs.describe().loc['count']
+
+mean_df.to_csv('mean.csv')
+
